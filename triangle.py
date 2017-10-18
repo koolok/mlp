9d77e6183ac768b0a33392fadcabd282f87c2d5b
koolok <koolok69@gmail.com>
2017-10-18 10:11:30 +0200

    Adding triangle.py who calculate and exporte a pre-computed distance matrix. It export in pickle : distance_matrix.pk

---
+++ triangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 12:55:54 2017
+
+@author: koolok
+"""
+
+from pylab import zeros
+import pickle 
+import editdistance
+
+
+def init() :
+    try :
+        data = open("data.txt", "r", encoding="utf-8")
+        base = []
+        for line in data :
+            digit,word = line.split("/")
+            base.append(word[0:-1])
+        data.close()
+    except :
+        base = []
+    return base 
+
+base = init()
+
+distance_matrix = zeros((len(base),len(base)))
+
+for i in range(len(base)) :
+    for j in range(len(base)) :
+        if i != j :
+            distance_matrix[i,j] = editdistance.eval(base[i],base[j])
+    
+file = open('distance_matrix.pk', 'wb') 
+
+pickle.dump(distance_matrix, file) 
