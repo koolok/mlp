45c0dc0f621fb355f96d6689e1374a8da9455a4a
koolok <koolok69@gmail.com>
2017-11-24 12:28:30 +0100

    Exhange distance <-> distance_. 	Cast cost variable to int.

--- distance.py
+++ distance.py
@@ -17,15 +17,18 @@ def pen_mat(a,b,c,d,e):
         mat[i_row] = tmp
         tmp.rotate(1)
     return mat
-    
-    
+
+
 pen_mat = pen_mat(0,1,2,3,4)
 pen_add_suppr = 2
 
-def distance_(seq1, seq2, max_dist=-1):
+def distance(seq1, seq2, max_dist=-1):
     if seq1 == seq2:
         return 0
-	
+
+    seq1 = seq1.replace('#','')
+    seq2 = seq2.replace('#','')
+
     len1, len2 = len(seq1), len(seq2)
     if max_dist >= 0 and abs(len1 - len2) > max_dist:
         return -1
@@ -36,27 +39,30 @@ def distance_(seq1, seq2, max_dist=-1):
     if len1 < len2:
         len1, len2 = len2, len1
         seq1, seq2 = seq2, seq1
-	
+
     column = array('L', range(0,2*(len2 + 1),2))
-	
+
     for x in range(1, len1 + 1):
         column[0] = x
         last = x - 1
         for y in range(1, len2 + 1):
             old = column[y]
             cost = pen_mat[ord(seq1[x - 1])-48][ord(seq2[y - 1])-48]
-            column[y] = min(column[y] + 2, column[y - 1] + 2, last + cost)
+            column[y] = min(column[y] + 2, column[y - 1] + 2, last + int(cost))
             last = old
         if max_dist >= 0 and min(column) > max_dist:
             return -1
-	
+
     if max_dist >= 0 and column[len2] > max_dist:
         return -1
     return column[len2]
 
-def distance(word1,word2) : 
+def distance_(word1,word2) :
+    word1 = word1.replace('#','')
+    word2 = word2.replace('#','')
+
     dico = { (-1,-1):0 }
-    
+
     for i,c_i in enumerate(word1) :
         dico[i,-1] = dico[i-1,-1]+pen_add_suppr
         dico[-1,i] = dico[-1,i-1]+pen_add_suppr
