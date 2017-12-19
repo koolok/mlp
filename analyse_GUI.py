3402d30e23590004bbf0a81c9db8cc97242ac601
koolok <koolok69@gmail.com>
2017-12-19 13:41:33 +0100

    Create analyse function for the GUI

---
+++ analyse_GUI.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 19 13:32:28 2017
+
+@author: koolok
+"""
+
+from multiprocessing import Pool
+from distanceC import distance_multi_GUI
+
+def analyse_multi(word,base,label,k=1) :
+    if len(base) == 0 :
+        return -1
+    if len(base) == 1 :
+        return label[0]
+        
+    pool = Pool()
+    
+    all_distance = pool.starmap_async(distance_multi_GUI, zip(base,[word]*len(base),label)).get()
+    
+    pool.close()
+
+#    all_distance = dict(zip(label, all_distance))
+#    
+#    all_distance = sorted(label, key=all_distance.__getitem__)
+
+    all_distance = sorted(all_distance, key=lambda tup: tup[1])
+    
+    votes = [0]*10
+    
+    Nearest = []
+    for i in range(k):
+        votes[all_distance[i][0]] +=1
+        Nearest.append(all_distance[i][2])
+                
+    return votes.index(max(votes))
--- distanceC.pyx
+++ distanceC.pyx
@@ -122,3 +122,58 @@ cpdef distance_multi(str seq1, str seq2, int label, int max_dist=-1):
     if max_dist >= 0 and column[len2] > max_dist:
         return -1
     return (label,column[len2])
+
+cpdef distance_multi_GUI(str seq1, str seq2, int label, int max_dist=-1):
+    if seq1 == seq2:
+        return 0
+#
+#    cdef char *seq1
+#    cdef char *seq2
+    
+    seq1 = seq1.replace('#','')
+    seq2 = seq2.replace('#','')
+    
+    cdef int len1
+    cdef int len2
+    
+    len1, len2 = len(seq1), len(seq2)
+    if max_dist >= 0 and abs(len1 - len2) > max_dist:
+        return -1
+    if len1 == 0:
+        return len2
+    if len2 == 0:
+        return len1
+    if len1 < len2:
+        len1, len2 = len2, len1
+        seq1, seq2 = seq2, seq1
+
+#    cdef array.array a = array.array('i', list(range(0,2*(len2 + 1),2)))
+#    cdef int[:] column = a
+
+    cdef list column
+    column = list(range(0,2*(len2 + 1),2))
+    
+#    cdef array.array b = array.array('i', [[0,1,2,3,4,3,2,1],[1,0,1,2,3,4,3,2],[2,1,0,1,2,3,4,3],[3,2,1,0,1,2,3,4],[4,3,2,1,0,1,2,3],[3,4,3,2,1,0,1,2],[2,3,4,3,2,1,0,1],[1,2,3,4,3,2,1,0]])
+#    cdef int[:] pen_mat = b
+
+    cdef list pen_mat
+    pen_mat = [[0,1,2,3,4,3,2,1],[1,0,1,2,3,4,3,2],[2,1,0,1,2,3,4,3],[3,2,1,0,1,2,3,4],[4,3,2,1,0,1,2,3],[3,4,3,2,1,0,1,2],[2,3,4,3,2,1,0,1],[1,2,3,4,3,2,1,0]]
+
+
+    cdef int x
+    cdef int y
+    for x in range(1, len1 + 1):
+        column[0] = x
+        last = x - 1
+        
+        for y in range(1, len2 + 1):
+            old = column[y]
+            cost = pen_mat[ord(seq1[x - 1])-48][ord(seq2[y - 1])-48]
+            column[y] = min(column[y] + 2, column[y - 1] + 2, last + int(cost))
+            last = old
+        if max_dist >= 0 and min(column) > max_dist:
+            return -1
+
+    if max_dist >= 0 and column[len2] > max_dist:
+        return -1
+    return (label,column[len2],seq1)
